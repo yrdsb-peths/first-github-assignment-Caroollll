75db16c7dd1e388a09340b6e35ff2ef9273dd1f5
Caroollll <349351197@gapps.yrdsb.ca>
2023-11-13 08:45:56 -0500

    updated student class

--- Student.java
+++ Student.java
@@ -1,15 +1,20 @@
 public class Student {
     private String firstName;
+	private String lastName;
 
-    public Student(String fName) {
+    public Student(String fName, String lName) {
         firstName = fName;
+		lastName = lName;
     }
 
-    public String getName() {
+    public String getFirstName() {
         return firstName;
     } 
 		
+	public String getLastName() {
+		return lastName;
+	} 
     public String toString() {
-        return "Your name is " + firstName; 
+        return "Your name is " + firstName + lastName; 
     }
 }
