c8687f72d2f7968e861c2b725bba0931f7cc5379
Caroollll <150150308+Caroollll@users.noreply.github.com>
2023-11-13 09:07:06 -0500

    Update indenting for Student.java

--- Student.java
+++ Student.java
@@ -1,20 +1,21 @@
 public class Student {
-    private String firstName;
-	private String lastName;
+private String firstName;
+private String lastName;
 
-    public Student(String fName, String lName) {
-        firstName = fName;
+	public Student(String fName, String lName) {
+        	firstName = fName;
 		lastName = lName;
-    }
+	}
 
-    public String getFirstName() {
-        return firstName;
-    } 
-		
+	public String getFirstName() {
+        	return firstName;
+	} 
+	
 	public String getLastName() {
 		return lastName;
 	} 
-    public String toString() {
-        return "Your name is " + firstName + lastName; 
-    }
+	
+	public String toString() {
+        	return "Your name is " + firstName + lastName; 
+	}
 }
